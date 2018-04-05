7f55c3dad4a0e0ab7829b68319669a92c9fe6bf5
jeongyuseock <fjsls@naver.com>
2018-04-05 16:23:22 +0900

    hi

--- src/main/scala/com/haiteam/Example_Join.scala
+++ src/main/scala/com/haiteam/Example_Join.scala
@@ -27,13 +27,65 @@ object Example_Join {
 
 
 // (임시)테이블생성 일단가볍게 쓰는용도? 이리저리
-  mainData1.createTempView("maindata")
-  subData1.createTempView("subdata")
+  mainData1.createOrReplaceTempView("maindata")
+  subData1.createOrReplaceTempView("subdata")
 
-
-  var leftJoinData = spark.sql("select a.*, b.productname " +
-    "from maindata a left outer join subdata b " +
+  var test3 = spark.sql("select a.productgroup, b.productname " +
+    "from maindata a " +
+    "left join subdata b " +
     "on a.productgroup = b.productid")
+
+
+  var leftJoinData =
+
+     //a.productgroup b.productid
+    var test2 = spark.sql("select a.regionid, a.productgroup, b.productname, a.yearweek, a.qty " +
+    "from maindata a " + " left join subdata b " +
+    "on a.productgroup = b.productid")
+
+
+
+  // 오라클 연결하기
+  var staticUrl = "jdbc:oracle:thin:@192.168.110.111:1521/orcl"
+  var staticUser = "kopo"
+  var staticPw = "kopo"
+  var selloutDb = "kopo_channel_seasonality_new"
+  var selloutDb2 = "kopo_region_mst"
+
+
+  //데이터 읽어오기
+  val selloutData= spark.read.format("jdbc").
+    options(Map("url" -> staticUrl, "dbtable" -> selloutDb, "user" -> staticUser, "password" -> staticPw)).load
+ //테이블생성
+  selloutData.createOrReplaceTempView("selloutTable")
+
+// 데이터 읽어오기
+  val selloutData2= spark.read.format("jdbc").
+    options(Map("url" -> staticUrl, "dbtable" -> selloutDb2, "user" -> staticUser, "password" -> staticPw)).load
+  //테이블생성
+  selloutData2.createOrReplaceTempView("selloutTable2")    //(sellouttable2) 가 생성한 테이블이름
+
+  selloutData.show()
+
+
+
+   spark.sql("select a.product, b.regionname " +
+    "from selloutTable a " +
+    "left join selloutTable2 b " +
+  "on a.regionid = b.regionid")
+
+  spark.sql("select a.regionid, b.regionname " +
+    "from selloutTable a " +
+    "inner join selloutTable2 b " +
+    "on a.regionid = b.regionid")
+
+
+
+
+
+
+
+
 
 
 
